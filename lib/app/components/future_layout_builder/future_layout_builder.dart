795bb14c4ddeb48b81b5d37f2963f081d49bad2b
Spicely <Spicely@outlook.com>
2024-12-18 16:03:35 +0800

    增加配置文件

---
+++ lib/app/components/future_layout_builder/future_layout_builder.dart
@@ -0,0 +1,112 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../theme_config/theme_config.dart';
+
+class FutureLayoutBuilderController {
+  _FutureLayoutBuilderState? _state;
+
+  FutureLayoutBuilderController();
+
+  /// 重载
+  void reload() {
+    _state?.reload();
+  }
+
+  /// 绑定状态
+  void _bindState(_FutureLayoutBuilderState state) {
+    _state = state;
+  }
+}
+
+class FutureLayoutBuilder<T> extends StatefulWidget {
+  final Widget Function(T) builder;
+
+  final Function()? future;
+
+  final FutureLayoutBuilderController? controller;
+
+  final ThemeConfig? config;
+
+  /// 是否需要拉取
+  final T? data;
+
+  const FutureLayoutBuilder({
+    super.key,
+    required this.builder,
+    this.future,
+    this.config,
+    this.data,
+    this.controller,
+  });
+
+  @override
+  State<FutureLayoutBuilder<T>> createState() => _FutureLayoutBuilderState<T>();
+}
+
+class _FutureLayoutBuilderState<T> extends State<FutureLayoutBuilder<T>> {
+  late Future<dynamic> _future;
+
+  ValueKey key = ValueKey(DateTime.now());
+
+  @override
+  initState() {
+    super.initState();
+    widget.controller?._bindState(this);
+    _future = onFuture();
+  }
+
+  @override
+  void didUpdateWidget(covariant FutureLayoutBuilder<T> oldWidget) {
+    if (widget.future != oldWidget.future) {
+      _future = onFuture();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    widget.controller?._bindState(this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.data != null) {
+      return widget.builder(widget.data!);
+    }
+    return FutureBuilder(
+      key: key,
+      future: _future,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data is Error || snapshot.data is DioException || snapshot.data is MissingPluginException) {
+            return widget.config?.loadFailWidget(context, snapshot.data, reload) ?? themeConfig.loadFailWidget(context, snapshot.data, reload);
+          } else {
+            return widget.builder(snapshot.data);
+          }
+        } else if (snapshot.hasError) {
+          return widget.config?.loadFailWidget(context, snapshot.data, reload) ?? themeConfig.loadFailWidget(context, snapshot.data, reload);
+        } else {
+          return widget.config?.loadingWidget(context) ?? themeConfig.loadingWidget(context);
+        }
+      },
+    );
+  }
+
+  void reload() async {
+    key = ValueKey(DateTime.now());
+    _future = onFuture();
+    setState(() {});
+  }
+
+  Future<dynamic> onFuture() async {
+    try {
+      dynamic res = await widget.future?.call();
+      return res ?? true;
+    } catch (e) {
+      return e;
+    }
+  }
+}
--- lib/app/data/utils/utils.dart
+++ lib/app/data/utils/utils.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart' hide Response;
 import 'package:logger/logger.dart';
 import 'package:open_file/open_file.dart';
@@ -13,21 +14,24 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../env/env.dart';
+import '../../components/cached_image/cached_image.dart';
 import '../../components/dialog/upgrade_dialog/upgrade_dialog.dart';
-import '../models/upgrade_model.dart';
+import '../../components/theme_config/theme_config.dart';
 import '../manager/compress_manager/compress_manager.dart';
 import '../manager/isar_manager/isar_manager.dart';
 import '../manager/system_manager/system_manager.dart';
 import '../mixins/permission_mixin.dart';
+import '../models/upgrade_model.dart';
 
 part '_apis.dart';
 part '_config.dart';
 part '_error.dart';
-part '_tools/_http.dart';
+part '_extend_theme_config.dart';
+part '_extend_upgrade.dart';
 part '_plugins.dart';
+part '_tools/_http.dart';
 part '_tools/_tools.dart';
 part '_tools/_upgrade.dart';
-part '_extend_upgrade.dart';
 
 class _Utils {
   _Utils._();
@@ -51,6 +55,7 @@ class _Utils {
     await plugins.init();
     await tools.init();
     apis.init();
+    _ExtendThemeConfig._().init();
   }
 }
 
