c3d797fc8a544648805d7d513629622d38d0d3c9
Aero <Spicely@outlook.com>
2025-03-14 10:43:06 +0800

    增加判断版本demo

--- lib/app/data/utils/_extend_upgrade.dart
+++ lib/app/data/utils/_extend_upgrade.dart
@@ -6,13 +6,37 @@ class _ExtendUpgrade extends _Upgrade {
   @override
   Future<UpgradeModel> checkUpgrade() async {
     // try {
-    //   GlobalController g = Get.find<GlobalController>();
-    //   Map<String, dynamic> res = await utils.apis.getLatestVersion(g.config.channel, g.config.projectId);
+    //  Map<String, dynamic> res = await utils.apis.getLatestVersion(channel, utils.config.projectId);
+
+    //   String version = res["versionCode"].toString().toUpperCase().replaceAll('V', '');
+
+    //   /// 本地
+    //   PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    //   String localVersion = packageInfo.version;
+
+    //   /// 比对
+    //   List<int> localVersionParts = localVersion.split('.').map(int.parse).toList();
+    //   List<int> serviceVersionParts = version.split('.').map(int.parse).toList();
+
+    //   int maxLength = max(localVersionParts.length, serviceVersionParts.length);
+    //   bool isUpgrade = false;
+
+    //   for (int i = 0; i < maxLength; i++) {
+    //     int localPart = i < localVersionParts.length ? localVersionParts[i] : 0;
+    //     int servicePart = i < serviceVersionParts.length ? serviceVersionParts[i] : 0;
+
+    //     if (servicePart > localPart) {
+    //       isUpgrade = true;
+    //       break;
+    //     } else if (servicePart < localPart) {
+    //       break;
+    //     }
+    //   }
 
     //   return UpgradeModel(
-    //     isUpgrade: utils.tools.isNotEmpty(res['versionCode']),
+    //     isUpgrade: isUpgrade,
     //     isForceUpgrade: res['isForce'] == 1,
-    //     isSilentUpgrade: res['isRenew'] == 1,
+    //     isSilentUpgrade: false,
     //     versionCode: res['versionCode'],
     //     fileUrl: res['fileUrl'],
     //     versionDesc: res['notice'],
