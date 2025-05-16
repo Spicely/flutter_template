f1651a3794f0df5aa02678e81a9b1f055f11104e
Aero <Spicely@outlook.com>
2025-05-16 18:32:47 +0800

    []

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -5,7 +5,13 @@ Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Initializing template...');
   final orgName = context.vars['org_name'];
 
+  final projectName = context.vars['project_name'];
+
   try {
+    context.logger.info('================  进入./${projectName}  ======================');
+    await Process.run('cd', ['./${orgName}'], runInShell: true);
+
+    context.logger.info('================  开始初始化模板  ======================');
     final result = await Process.run('flutter', ['create', '.', '--org', orgName], runInShell: true);
 
     if (result.exitCode != 0) {
