fc89dcb725f75b180fd4ed64d58b8ff175d92588
Aero <Spicely@outlook.com>
2025-05-16 21:08:22 +0800

    修复可能出现的大小不匹配

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -5,11 +5,11 @@ Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Initializing template...');
   final orgName = context.vars['org_name'];
 
-  final projectName = context.vars['project_name'];
+  String projectName = context.vars['project_name'];
 
   try {
     context.logger.info('================  开始初始化模板  ======================');
-    final result = await Process.run('cd', ['./${projectName}', '&&', 'flutter', 'create', '.', '--org', orgName], runInShell: true);
+    final result = await Process.run('cd', ['./${projectName.toLowerCase()}', '&&', 'flutter', 'create', '.', '--org', orgName], runInShell: true);
 
     if (result.exitCode != 0) {
       context.logger.err(result.stderr);
